f4418ff40cd36f2909fbd85e90712c737cbf2e7f
Matt Stammers <matthewstammers@googlemail.com>
2017-09-05 19:31:42 +0100

    Create Manual_Agents.py

---
+++ Manual_Agents.py
@@ -0,0 +1,35 @@
+# These are the very basic first level search agents in the Berkeley PacMan AI Course (CS188)
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+    
+def depthFirstSearch(problem):
+    """
+    Search the deepest nodes in the search tree first.
+
+    Your search algorithm needs to return a list of actions that reaches the
+    goal. Make sure to implement a graph search algorithm.
+
+    To get started, you might want to try some of these simple commands to
+    understand the search problem that is being passed in:
+
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    """
+    "*** YOUR CODE HERE ***"
+    from game import Directions
+    print "Start:", problem.getStartState()
+    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+    print "Start's successors:", problem.getSuccessors(problem.getStartState())
+    s = Directions.SOUTH
+    w = Directions.WEST
+    n = Directions.NORTH
+    return  [s, s, w, s, w, w, n, s, s, w, s, w]
